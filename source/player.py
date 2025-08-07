068a42407daa747e1dd80d8ff7f95e6318fa742c
rafaelviolet <86896648+rafaelvlt@users.noreply.github.com>
2025-08-07 14:11:39 -0300

    mudado a movimentação para utilizar vetores

--- source/player.py
+++ source/player.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, sheet_player):
@@ -9,34 +10,25 @@ class Player(pygame.sprite.Sprite):
         sheet_player: Imagem.
         """
         super().__init__()
-        self.x = x
-        self.y = y
-        self.speed = 5
+        self.direcao = pygame.math.Vector2()
+        self.velocidade = 500
         self.frame = pygame.image.load(sheet_player).convert_alpha()
         self.image = self.frame
-        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        self.rect = self.image.get_frect(center = (altura_tela/2, largura_tela/2))
 
-    def update(self, keys):#movimentacao do player
-        dx, dy = 0,0
-        if keys[pygame.K_w]: #cima
-            dy = -self.speed
-        
-        elif keys[pygame.K_s]: #baixo
-            dy = self.speed
+    def update(self, keys, delta_time):#movimentacao do player
+        #delta_time serve pra o jogador sempre se mover na mesma velocidade independente do fps
 
-        if keys[pygame.K_a]: #esquerda
-            dx = -self.speed
+        #muda os vetores se eles estão sendo pressionados ou não
+        #direita = 1, esquerda = -1, cima = 1, baixo = -1
+        #se a tecla está sendo pressionada, ela é True, true quando convertido pra int é 1
+        self.direcao.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direcao.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        #caso a direção não for parado(dá erro), normaliza o vetor para que ao se mover na diagonal, não se mova mais rápido
+        if self.direcao != (0,0):
+            self.direcao = self.direcao.normalize()
 
-        elif keys[pygame.K_d]: #direita
-            dx = self.speed
-
-        if dx != 0 and dy != 0: #igualar velocidade na diagonal
-            dx *= 0.7071
-            dy *= 0.7071
-
-        self.x += dx
-        self.y += dy
-        self.rect.topleft = (self.x, self.y) #atualiza o x e y atual
+        self.rect.center +=  self.direcao * self.velocidade * delta_time #atualiza a posição atual
 
    
 
