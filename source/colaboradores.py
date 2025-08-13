f6f73dd99bcfeb350bbef1eff8f433f6014b071b
Álvaro Lima <amol@cin.ufpe.br>
2025-08-13 11:23:10 -0300

    acrescentado o background dos colaboradores, falta por o nome correto de cada um no código, podem ficar a vontade para realizar alterações!!!

--- source/colaboradores.py
+++ source/colaboradores.py
@@ -1,4 +1,5 @@
 import pygame
+from os.path import join
 
 class Colaboradores:
     def __init__(self, game):
@@ -25,6 +26,10 @@ class Colaboradores:
         self.scroll_y = 0
         self.scroll_speed = 50  # pixels por segundo
 
+        # --- Carrega a imagem de fundo ---
+        self.background = pygame.image.load(join('assets', 'img', 'colaboradores.jpeg')).convert()
+        self.background = pygame.transform.scale(self.background, (game.tela.get_width(), game.tela.get_height()))
+
     def iniciar_input(self):
         """Reseta o estado quando entra na tela"""
         self.scroll_y = 0
@@ -41,7 +46,8 @@ class Colaboradores:
         self.scroll_y -= self.scroll_speed * delta_time
 
     def draw(self, surface):
-        surface.fill((0, 0, 0))
+        # --- Desenha a imagem de fundo ---
+        surface.blit(self.background, (0, 0))
 
         # Título
         titulo = self.font_title.render("Colaboradores", True, (255, 255, 0))
