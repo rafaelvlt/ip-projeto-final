9810b8c48e6881b49c3b64ddcfe69ab8e2ac567c
eric <ericsantiago77@gmail.com>
2025-08-12 22:09:15 -0300

    add mapa

--- assets/map/tilemap_packed.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tilemap_packed" tilewidth="16" tileheight="16" tilecount="486" columns="27">
- <image source="//wsl.localhost/Ubuntu/home/violet/Faculdade/IP/ip-projeto-final/assets/maps/tilemap_packed.png" width="432" height="288"/>
-</tileset>
